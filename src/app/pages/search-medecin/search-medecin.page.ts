a9d4c0439d0296c55457b8dda13738c83eaf84c2
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-11-03 16:39:56 +0100

    search product sending

--- src/app/pages/search-medecin/search-medecin.page.ts
+++ src/app/pages/search-medecin/search-medecin.page.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from '../../services/auth.service';
 import { NavController , ModalController } from '@ionic/angular';
-
+import { SearchMedecinService } from '../../services/search/search-medecin.service';
 @Component({
   selector: 'app-search-medecin',
   templateUrl: './search-medecin.page.html',
@@ -11,7 +11,8 @@ import { NavController , ModalController } from '@ionic/angular';
 export class SearchMedecinPage implements OnInit {
 
   currentUser: User;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+              private searchService: SearchMedecinService) { }
 
   ngOnInit() {
     this.getCurrentUser();
@@ -28,6 +29,7 @@ export class SearchMedecinPage implements OnInit {
   onInput(value){
       const medecins = {products : value.split(',').map(med => {return ({ name : med })})};
       console.log(medecins);
+      this.searchService.searchProducts(medecins);
   }
 
   onClear(event){
--- src/app/services/search/search-medecin.service.ts
+++ src/app/services/search/search-medecin.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import { config } from '../config';
 @Injectable({
   providedIn: 'root'
 })
 export class SearchMedecinService {
 
-  constructor() { }
+  url = config.baseURL;
+  constructor(private httpClient: HttpClient) { }
+
+
+  searchProducts(products){
+    return new Promise((resolve,reject) => {
+      this.httpClient.post(this.url + 'crm/productsmanagement/searchproducts', products)
+        .subscribe(result => {
+          console.log(result);
+        }, err => {
+          console.log(err);
+        });
+    })
+  }
+
+
 }
