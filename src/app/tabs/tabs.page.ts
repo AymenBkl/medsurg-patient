df4111112f13dc450e006ea983a3fc82beb33688
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-31 13:11:24 +0100

    prescription add

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalControllers } from '../classes/modalController';
+import { User } from '../interfaces/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  modalControllers: ModalControllers;
+  currentUser:User;
+  constructor(private modalCntrl: ModalController,
+              private authService: AuthService) {
+    this.currentUser = this.authService.user;
+    this.modalControllers = new ModalControllers(modalCntrl);
+  }
 
-  constructor() {}
+  callPrescription(){
+    this.modalControllers.addPrescription(this.currentUser, []);
+  }
 
 }
