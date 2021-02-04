d09a772e83dad03fcf142a3b8d4f520bd4002cb4
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-02-04 18:44:42 +0100

    reset password form

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../register/must-matchValdiator';
+import { onValueChanged } from './valueChanges';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  resetPasswordForm: FormGroup;
+  formErrors: any;
+  submitted = false;
+  validationErrors: {errmsg , errcode};
+  constructor(private formBuilder: FormBuilder) {
+      this.buildResetPasswordForm();
+   }
 
   ngOnInit() {
   }
+
+  buildResetPasswordForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      password : ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword : ['', [Validators.required, Validators.minLength(6)]],
+    },
+    {
+      validators : MustMatch('password', 'confirmPassword')
+    });
+    this.resetPasswordForm.valueChanges
+      .subscribe(resetPassword => {
+        this.formErrors = onValueChanged(resetPassword, this.resetPasswordForm);
+        console.log(resetPassword);
+      });
+  }
+
+  resetPassword(){
+    console.log(this.resetPasswordForm.value);
+  }
+
 
 }
